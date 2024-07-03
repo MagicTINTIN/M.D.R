cb5ecb0016d278bf28ebdfe2371a57e47a01a9af
MagicTINTIN <valentinservieres@gmail.com>
2024-07-03 21:50:36 +0200

    add more animations modes

--- source/main.cc
+++ source/main.cc
@@ -6,6 +6,7 @@
 #include <chrono>
 #include <csignal>
 #include <vector>
+#include <random>
 #include <stop_token>
 #include <unistd.h>
 #include <pthread.h>
@@ -15,11 +16,10 @@
 #include "midi/xtouch.hh"
 #include "midi/animations.hh"
 
-#define NUMBER_OF_MODES 2
+#define NUMBER_OF_MODES 4
 int globalMode = NUMBER_OF_MODES - 1;
 int globalSpeed = 127;
 std::vector<std::thread> globalActiveThreads = {};
-
 
 int mapRange(int x, int inMin, int inMax, int outMin, int outMax)
 {
@@ -94,19 +94,143 @@ void randomAnim(Controller *surface)
     surface->animRandomOfVector({70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90}, -1, 500, 0);
 }
 
-int modesChanger(Controller *surface, int val) {
-    if (val == 0) return 0;
+int getFaderAlertSin(int x, int &amp, int &period, int &ampOffset, int const &timeOffset)
+{
+    return amp * std::sin(2 * M_PI * (x + timeOffset * 3) / period) + ampOffset;
+}
+
+void redAlarm(Controller *surface)
+{
+    int timer = 0;
+    int rndOffset = 30 + std::rand() % 67;
+    int rndAmp = 10 + std::rand() % 15;
+    int rndPeriod = 5 + std::rand() % 60;
+    int period = 60;
+    int memory = 81;
+    int faders[memory] = {0};
+    int topdanger = 0;
+    int topDangerPeriod = 20;
+    int dispersion = 5;
+    while (1)
+    {
+        if (timer % period == 0)
+        {
+            surface->setLCDColor({2, 4, 6, 8}, XTOUCH_SCREEN_BLACK);
+            surface->setLCDColor({1, 3, 5, 7}, XTOUCH_SCREEN_RED);
+
+            rndPeriod = 20 + std::rand() % 100;
+        }
+        if (timer % period == period / 4)
+        {
+            surface->allLightsRed(XTOUCH_STATUS_ON);
+        }
+        if (timer % period == period / 2)
+        {
+            surface->setLCDColor({1, 3, 5, 7}, XTOUCH_SCREEN_BLACK);
+            surface->setLCDColor({2, 4, 6, 8}, XTOUCH_SCREEN_RED);
+        }
+        if (timer % period == 3 * period / 4)
+        {
+            surface->allLightsRed(XTOUCH_STATUS_OFF);
+        }
+        if (timer % 9 == 0)
+        {
+            surface->setLight({61, 69, 83, 90}, XTOUCH_STATUS_ON);
+            if (timer % 120 < 40) {
+                surface->setLight({8,11,12,15}, XTOUCH_STATUS_ON);
+            }
+        }
+        if (timer % 9 == 4)
+            surface->setLight({61, 69, 83, 90, 8,11,12,15}, XTOUCH_STATUS_OFF);
+
+        if (timer % 600 == 0)
+        {
+            rndOffset = 30 + std::rand() % 34;
+            rndAmp = 10 + std::rand() % 15;
+        }
+        if (timer % 80 < 20)
+            surface->setRing({1, 4, 5, 8}, {XTOUCH_RING_MODE_DISPERSION_WEXT, XTOUCH_RING_MODE_NORMAL, XTOUCH_RING_MODE_NORMAL, XTOUCH_RING_MODE_DISPERSION_WEXT}, {dispersion, 0, 0, dispersion});
+        else if (timer % 80 < 40)
+            surface->setRing({2, 1, 8, 7}, {XTOUCH_RING_MODE_DISPERSION_WEXT, XTOUCH_RING_MODE_NORMAL, XTOUCH_RING_MODE_NORMAL, XTOUCH_RING_MODE_DISPERSION_WEXT}, {dispersion, 0, 0, dispersion});
+        else if (timer % 80 < 60)
+            surface->setRing({3, 2, 7, 6}, {XTOUCH_RING_MODE_DISPERSION_WEXT, XTOUCH_RING_MODE_NORMAL, XTOUCH_RING_MODE_NORMAL, XTOUCH_RING_MODE_DISPERSION_WEXT}, {dispersion, 0, 0, dispersion});
+        else
+            surface->setRing({4, 3, 6, 5}, {XTOUCH_RING_MODE_DISPERSION_WEXT, XTOUCH_RING_MODE_NORMAL, XTOUCH_RING_MODE_NORMAL, XTOUCH_RING_MODE_DISPERSION_WEXT}, {dispersion, 0, 0, dispersion});
+        if (timer % 20 == 0)
+        {
+            dispersion = 5;
+        }
+        if (timer % 4 == 0) dispersion--;
+        for (size_t i = 0; i < 8; i++)
+        {
+            faders[i + 1] = faders[i];
+        }
+        faders[timer % memory] = getFaderAlertSin(timer, rndAmp, rndPeriod, rndOffset, 0);
+
+        if (timer % 4 == 0)
+        {
+            topdanger++;
+            if ((topdanger % topDangerPeriod) < XTOUCH_ROWS[0].size() * 2)
+            {
+                surface->setLight(XTOUCH_ROWS[0][topdanger % topDangerPeriod], XTOUCH_STATUS_ON);
+            }
+            else
+            {
+                surface->setLight(XTOUCH_ROWS[0][(topdanger - XTOUCH_ROWS[0].size() * 2) % topDangerPeriod], XTOUCH_STATUS_OFF);
+            }
+        }
+
+        surface->setFader(XTOUCH_FADERS, {faders[(timer) % memory],
+                                          faders[(timer + memory / 9) % memory],
+                                          faders[(timer + 2 * memory / 9) % memory],
+                                          faders[(timer + 3 * memory / 9) % memory],
+                                          faders[(timer + 4 * memory / 9) % memory],
+                                          faders[(timer + 5 * memory / 9) % memory],
+                                          faders[(timer + 6 * memory / 9) % memory],
+                                          faders[(timer + 7 * memory / 9) % memory],
+                                          faders[(timer + 8 * memory / 9) % memory]});
+
+        usleep(1000000 / period);
+        timer++;
+        // timer %= period;
+    }
+}
+
+void greenChaser(Controller *surface)
+{
+    while (1)
+    {
+        usleep(2000000);
+        surface->animFilledVector(XTOUCH_BTN_GREEN, 60, 0);
+        surface->setLight({0,7}, {127,127});
+        usleep(100000);
+        surface->setLight({0,7}, {0,0});
+    }
+    
+}
+
+int modesChanger(Controller *surface, int val)
+{
+    if (val == 0)
+        return 0;
 
     globalMode++;
-    globalMode%=NUMBER_OF_MODES;
+    globalMode %= NUMBER_OF_MODES;
     std::cout << "cleaning threads...\n";
-    for (auto& thread : globalActiveThreads) {
+    for (auto &thread : globalActiveThreads)
+    {
         pthread_cancel(thread.native_handle());
     }
+    surface->resetFunctionsToTrigger();
+    surface->resetValuesToUpdate();
     surface->globalReset();
     std::cout << "=> MODE : " << globalMode << std::endl;
+    surface->addFunctionToTrigger({TRIGGER_BUTTON_TYPE, XTOUCH_SCRUB, &modesChanger});
 
-    if (globalMode == 0) {
+    if (globalMode == 0)
+    {
+        surface->addValueToUpdate({TRIGGER_FADER_TYPE, XTOUCH_FADER_1_CH, &globalSpeed});
+        surface->addFunctionToTrigger({TRIGGER_FADER_TYPE, XTOUCH_FADER_1_CH, &updateOtherFaders});
         surface->setLCDFullLineText(0, "Commandes moteurs principaux");
         surface->setLCDColor({1, 8}, XTOUCH_SCREEN_YELLOW);
         surface->setLCDColor({3, 4, 5, 6}, XTOUCH_SCREEN_WHITE);
@@ -118,10 +242,19 @@ int modesChanger(Controller *surface, int val) {
         globalActiveThreads.emplace_back(randomAnim, surface);
     }
     else if (globalMode == 1) {
-        surface->setLCDFullLineText(0, "ALERTE");
-
-        surface->setLCDColor({1, 3, 5, 7}, XTOUCH_SCREEN_RED);
+        surface->setLCDColor({1, 8}, XTOUCH_SCREEN_GREEN);
+        surface->setLCDColor({4, 5}, XTOUCH_SCREEN_BLUE);
+        surface->setLight(XTOUCH_ZOOM, XTOUCH_STATUS_BLINK);
+        surface->setFader(XTOUCH_FADERS, {90,74,65,60,55,60,65,74,90});
+        globalActiveThreads.emplace_back(greenChaser, surface);
     }
+    else if (globalMode == 2)
+    {
+        surface->setLCDText({1, 3, 5, 7}, {0, 0, 0, 0}, "ALERT!");
+        surface->setLCDText({2, 4, 6, 8}, {0, 0, 0, 0}, "DANGER");
+        globalActiveThreads.emplace_back(redAlarm, surface);
+    }
+    
     return 0;
 }
 
@@ -146,10 +279,7 @@ int main()
     Controller surface(surfaceName, dInputId, dOutputId);
 
     // adding values to be updated
-    surface.addValueToUpdate({TRIGGER_FADER_TYPE, XTOUCH_FADER_1_CH, &globalSpeed});
-    surface.addFunctionToTrigger({TRIGGER_FADER_TYPE, XTOUCH_FADER_1_CH, &updateOtherFaders});
     surface.addFunctionToTrigger({TRIGGER_BUTTON_TYPE, XTOUCH_SCRUB, &modesChanger});
-
     surface.globalReset();
 
     // surface.setSegmentsChar({9, 8, 7, 6, 5, 4, 3}, {'b', 'o', 'n', 'j', 'o', 'u', 'r'});
@@ -161,7 +291,6 @@ int main()
     // surface.setLCDFullLineText(1,"C'est marrant !");
     // surface.setLCDText(7, 1, "Coucou");
     // surface.setLCDText(8, 0, "Yo!", 1);
-    
 
     surface.startThreads();
 
